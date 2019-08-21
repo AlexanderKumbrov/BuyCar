11a3a6d53c9af05731eb036c639c43247a920b83
Alexander Kumbrov <alexandr.k@irokez.me>
2019-08-21 15:45:08 +0300

    Start

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().go();
+    }
+    public void go(){
+        parseMainPage();
+
+    }
+    public void parseMainPage(){
+        try {
+            Document doc = Jsoup.connect("https://www.mashina.kg/") //Document jsoup
+                    .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                    .referrer("http://www.google.com")
+                    .get();
+            Elements listCars = doc.select("div.our-choice.clr");
+            //save information in file.txt (temporarily)
+            FileWriter writer = new FileWriter("Cars.txt");
+
+            for(Element element : listCars.select("a")){
+                writer.write(element.text() + "\n");
+                System.out.println(element.text());
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
