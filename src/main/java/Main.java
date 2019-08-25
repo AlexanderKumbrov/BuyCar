a8a5662df84cc9b2d2a083a89225428e1f34c6cb
Alexander Kumbrov <alexandr.k@irokez.me>
2019-08-25 15:34:19 +0300

    send ArrayList cars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,28 @@ import org.jsoup.select.Elements;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class Main {
-    private String car;
+    private ArrayList<String> listCars = new ArrayList<String>();
     public static void main(String[] args) {
         new Main().go();
     }
     public void go(){
         parseMainPage();
+
         try {
             ServerSocket serverSocket = new ServerSocket(4242);
             while (true){
                 Socket socket = serverSocket.accept();
-                PrintWriter writer = new PrintWriter(socket.getOutputStream());
-                writer.println(car);
-                writer.close();
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                outputStream.writeObject(listCars);
+
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-
 
     }
     public void parseMainPage(){
@@ -35,14 +36,14 @@ public class Main {
                     .userAgent("Chrome/4.0.249.0 Safari/532.5")
                     .referrer("http://www.google.com")
                     .get();
-            Elements listCars = doc.select("div.cars-container");
+            Elements listCarsElements = doc.select("div.cars-container");
             //save information in file.txt (temporarily)
             FileWriter writer = new FileWriter("Cars.txt");
 
-            for(Element element : listCars.select("a")){
-                writer.write(element.text() + "\n");
-                car = element.text();
-                System.out.println(element.text());
+            for(Element element : listCarsElements.select("a")){
+               listCars.add(element.text() + "\n");
+               writer.write(element.text() + "\n");
+               System.out.println(element.text());
             }
             writer.close();
 
